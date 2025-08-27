c80dc2c8f38ae6aa7c9aa0b91d8a5a8087e40e57
Shin'ichiro Suzuki <shin@sforzando.co.jp>
2025-08-27 20:35:01 +0900

    feat: :+1: Add format and lint checks to CI and deploy workflows

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -4,13 +4,16 @@ import type { Handle } from '@sveltejs/kit';
 import { paraglideMiddleware } from '$lib/paraglide/server';
 
 const handleParaglide: Handle = ({ event, resolve }) =>
-	paraglideMiddleware(event.request, ({ request, locale }: { request: Request; locale: string }) => {
-		event.request = request;
+	paraglideMiddleware(
+		event.request,
+		({ request, locale }: { request: Request; locale: string }) => {
+			event.request = request;
 
-		return resolve(event, {
-			transformPageChunk: ({ html }) => html.replace('%paraglide.lang%', locale)
-		});
-	});
+			return resolve(event, {
+				transformPageChunk: ({ html }) => html.replace('%paraglide.lang%', locale)
+			});
+		}
+	);
 
 const handleAuth: Handle = async ({ event, resolve }) => {
 	const sessionToken = event.cookies.get(auth.sessionCookieName);
