0314ed1138becf9c4d5c6d28998624228512b74a
Shin'ichiro Suzuki <shin@sforzando.co.jp>
2025-08-28 19:46:20 +0900

    feat: :+1: Update Playwright config for development server settings

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
 	webServer: {
-		command: 'npm run build && npm run preview',
-		port: 4173
+		command: 'npm run build && npm run dev',
+		port: 5173,
+		reuseExistingServer: !process.env.CI
 	},
 	testDir: 'e2e'
 });
