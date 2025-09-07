788cf13af0ed329068ea455fd08afedda4a52c9c
Shin'ichiro Suzuki <shin@sforzando.co.jp>
2025-09-08 02:09:41 +0900

    wip: :construction: preparing Storybooks

---
+++ src/stories/components/Footer.stories.svelte
@@ -0,0 +1,19 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import Footer from '$lib/components/Footer.svelte';
+
+	const { Story } = defineMeta({
+		title: 'Components/Footer',
+		component: Footer,
+		parameters: {
+			layout: 'fullscreen'
+		},
+		tags: ['autodocs']
+	});
+</script>
+
+<Story name="Default" />
+
+<Story name="Mobile View" parameters={{ viewport: { defaultViewport: 'mobile1' } }} />
+
+<Story name="Tablet View" parameters={{ viewport: { defaultViewport: 'tablet' } }} />
---
+++ src/stories/components/Header.stories.svelte
@@ -0,0 +1,19 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import Header from '$lib/components/Header.svelte';
+
+	const { Story } = defineMeta({
+		title: 'Components/Header',
+		component: Header,
+		parameters: {
+			layout: 'fullscreen'
+		},
+		tags: ['autodocs']
+	});
+</script>
+
+<Story name="Default" />
+
+<Story name="Mobile View" parameters={{ viewport: { defaultViewport: 'mobile1' } }} />
+
+<Story name="Tablet View" parameters={{ viewport: { defaultViewport: 'tablet' } }} />
